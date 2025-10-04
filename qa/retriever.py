ea09b5d831dd5537f469b40570c0dd0cb8e40041
tinsaeabreham45 <111001136+tinsaeabreham45@users.noreply.github.com>
2025-10-04 12:24:53 +0300

    make only top 3

--- qa/retriever.py
+++ qa/retriever.py
@@ -1,6 +1,6 @@
 from .vectorstore import load_vectorstore
 
-def get_retriever(persist_dir="chroma_db", k=10):
+def get_retriever(persist_dir="chroma_db", k=3):
     vectorstore = load_vectorstore(persist_dir)
     retriever = vectorstore.as_retriever(search_kwargs={"k": k})
     return retriever
